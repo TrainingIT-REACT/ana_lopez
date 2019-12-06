31f36325ef27a6bca321d10c550b80b1901a0003
anuska87 <nitta_ea_87@hotmail.com>
2019-12-06 17:52:37 +0100

    Refactor Menu to a functional component

--- src/App/Menu.js
+++ src/App/Menu.js
@@ -5,12 +5,12 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { DRAWER_WIDTH } from './constants';
 import menuItems from './menuItems';
 import { Typography } from '@material-ui/core';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   drawer: {
     width: DRAWER_WIDTH,
     flexShrink: 0
@@ -25,37 +25,35 @@ const styles = theme => ({
     minWidth: 35,
     color: theme.palette.primary.contrastText
   }
-});
+}));
 
-class Menu extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {menuItems.map(menuItem => {
-            const { Icon, text } = menuItem;
-            return (
-              <ListItem button key={text}>
-                <ListItemIcon className={classes.menuItemIcon}>
-                  <Icon />
-                </ListItemIcon>
-                <ListItemText primary={<Typography>{text}</Typography>} />
-              </ListItem>
-            );
-          })}
-        </List>
-      </Drawer>
-    );
-  }
-}
+const Menu = () => {
+  const classes = useStyles();
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {menuItems.map(menuItem => {
+          const { Icon, text } = menuItem;
+          return (
+            <ListItem button key={text}>
+              <ListItemIcon className={classes.menuItemIcon}>
+                <Icon />
+              </ListItemIcon>
+              <ListItemText primary={<Typography>{text}</Typography>} />
+            </ListItem>
+          );
+        })}
+      </List>
+    </Drawer>
+  );
+};
 
-export default withStyles(styles)(Menu);
+export default Menu;
