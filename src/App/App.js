f8d85e286b85e0992bc0da4d207bbb9b4210cab6
anuska87 <nitta_ea_87@hotmail.com>
2019-12-06 17:51:55 +0100

    Add recommendation of albums

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import { withStyles } from '@material-ui/core/styles';
 import Menu from './Menu';
+import AlbumCard from './components/AlbumCard';
 import { DRAWER_WIDTH } from './constants';
 
 const styles = theme => ({
@@ -20,7 +23,25 @@ const styles = theme => ({
     backgroundColor: theme.palette.grey[100],
     height: '100%'
   },
-  toolbar: theme.mixins.toolbar
+  toolbar: theme.mixins.toolbar,
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  },
+  recommendationList: {
+    marginTop: 10
+  },
+  albumesList: {
+    marginTop: 8,
+    width: '100%',
+    display: 'flex'
+  },
+  cardContainer: {
+    flex: 1,
+    marginRight: 8
+  }
 });
 
 class App extends Component {
@@ -47,23 +68,61 @@ class App extends Component {
     }
   }
 
+  getRandomAlbums = () => {
+    const { albums } = this.state;
+    const numberOfAlbums = albums.length;
+    if (numberOfAlbums !== 0) {
+      let recommendations = [];
+      while (recommendations.length < 3) {
+        const recommendationIndex = Math.floor(Math.random() * numberOfAlbums);
+        const albumRecommendation = albums[recommendationIndex];
+        if (!recommendations.find(recommendation => recommendation.id === albumRecommendation.id)) {
+          recommendations.push(albumRecommendation);
+        }
+      }
+      return recommendations;
+    }
+
+    return [];
+  };
+
   render() {
     const { classes } = this.props;
+    const albumRecommendations = this.getRandomAlbums();
     return (
       <div className={classes.root}>
         <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
+          <Toolbar className={classes.toolbar}>
             <Typography variant="h6">Reactify</Typography>
           </Toolbar>
         </AppBar>
         <Menu />
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          <Typography variant="h6">Main page</Typography>
+          {!this.state.loading && (
+            <>
+              <Typography variant="subtitle2">Hoy te recomendamos...</Typography>
+              <div className={classes.recommendationList}>
+                <Typography variant="h6">Álbumes</Typography>
+                <Divider />
+                <div className={classes.albumesList}>
+                  {albumRecommendations.map(recommendation => (
+                    <div className={classes.cardContainer}>
+                      <AlbumCard album={recommendation} />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
         </main>
       </div>
     );
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(App);
---
+++ src/App/components/AlbumCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  media: {
+    height: 140
+  }
+});
+
+const AlbumCard = props => {
+  const classes = useStyles();
+  const { album } = props;
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={album.cover} />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2">
+            {album.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            de {album.artist}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+AlbumCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+  album: PropTypes.object.isRequired
+};
+
+export default AlbumCard;
